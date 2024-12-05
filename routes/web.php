57b3cafadb165f9908ec7d97af9706381757ec3a
Juliandos <95juliandos@gmail.com>
2024-12-05 16:34:10 -0600

    Renderiza tabla de cocktails en el cliente

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Http;
+use Illuminate\Http\Request;
+
+use App\Models\Cocktail;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,4 +19,56 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/cocktails-api', function () {
+    $url = 'https://www.thecocktaildb.com/api/json/v1/1/random.php';
+    $cocktails = [];
+
+    // Realizar 5 solicitudes
+    for ($i = 0; $i < 5; $i++) {
+        $response = Http::get($url);
+
+        if ($response->ok()) {
+            $data = $response->json()['drinks'][0]; // Leer el primer cóctel
+            $cocktails[] = [
+                'id' => $data['idDrink'],
+                'name' => $data['strDrink'],
+                'category' => $data['strCategory'],
+                'alcoholic' => $data['strAlcoholic'],
+                'glass' => $data['strGlass'],
+                'instructions' => $data['strInstructions'],
+            ];
+        }
+    }
+
+    // Pasar los datos a la vista
+    return view('cocktails.api', compact('cocktails'));
+});
+
+Route::post('cocktails', function (Request $request) {
+    // $validated = $request->validate([
+    //     'id' => 'required|integer|unique:cocktail,id',
+    //     'nombre' => 'required|string|max:255',
+    //     'categoria' => 'required|string|max:255',
+    //     'alcoholica' => 'required|boolean',
+    //     'vaso' => 'required|string|max:255',
+    //     'instrucciones' => 'required|string',
+    // ]);
+
+    // Crear un nuevo registro en la base de datos
+    // $cocktail = Cocktail::create([
+    //     'id' => $request->input('id'),
+    //     'nombre' => $request->input(key: 'nombre'),
+    //     'categoria' => $request->input(key: 'categoria'),
+    //     'alcoholica' => $request->input('alcoholica'),
+    //     'vaso' => $request->input('vaso'),
+    //     'instrucciones' => $request->input('instrucciones')
+    // ]);
+
+    // Retornar una respuesta exitosa
+    return response()->json([
+        'message' => 'Cóctel guardado exitosamente.',
+        'cocktail' => "ABC",
+    ], 201);
 });
